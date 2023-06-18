d2ec98c2dc9767b993c129cb917f4df322f5b6c4
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-18 12:19:59 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [click, setClick] = useState(0)
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {click} times</p>
+        <button onClick={()=>(setClick(click+1))}>Click Me</button>
     </div>
   )
 }
